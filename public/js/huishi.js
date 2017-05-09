46247f98091ee9ed2c4ef573eb7811684afffb51
zhougogo <1290612478@qq.com>
2017-05-09 13:51:13 +0800

    add
    同步

--- public/js/huishi.js
+++ public/js/huishi.js
@@ -1,63 +1,40 @@
-//第一页的确定按钮
-
-//	$('.page1 .btm').click(function(){
-//		$('.page2').removeClass('hidden').siblings().addClass('hidden');
-//	})
-//$('.subject p').click(function() {
-//
-//	$(this).addClass('yesClick').siblings().removeClass('yesClick');
-//
-//})
-//$('.popup .cancelBtm').click(function() {
-//
-//	$(this).parents('.popup').addClass('hidden');
-//
-//})
-//
-//$('.page3 .popup .cancel').click(function() {
-//
-//	$('.page3 .popup').css('display', 'none');
-//
-//})
 
 count();
 function count(){
 	
-	$('.page0').fadeOut(5000);
+	$('.page0').fadeOut(4000);
 	
-	$('.page1').fadeIn(5000);
+	$('.page1').fadeIn(4000);
 	
-//	console.log($('.page2 .errPopup'));
+
 	
 	$('.page2 .errPopup').click(function(){
 		
 		$(this).hide();
 	})
+
+
+	var answerText = document.getElementsByClassName('subjectAll')[0];
 	
-	$('.page2 .answer p').click(function(){
-		
-//		console.log(123);
-		
-		$(this).css('background-color','#d7d7d6').siblings().css('background-color','white');
-	})
+	var answer = answerText.getElementsByTagName('p');
 	
+	for(var i = 0;i < answer.length;i++){
+		
+		answer[i].addEventListener('touchstart',function(){
+				
+			for(var j = 0;j<answer.length;j++){
+				
+				answer[j].style.backgroundColor = 'white';
+				
+			}
+			this.style.backgroundColor = "#d7d7d6";
+			
+		})
+		
+
+	}
 }
 
 
 
 
-//console.log(123);
-//question();
-//
-//function question(){
-//	var width = $('.subject .answer p').width();
-//	
-////	console.log(width);
-//	$('.subject .answer').css({
-//		'width':width,
-//		'left':'0%',
-//		'right':'0%',
-//		'margin':'auto'
-//		
-//	})
-//}
