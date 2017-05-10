7ce6fa1ca9d89d49392d2375d5aa6add41d9014b
zhougogo <1290612478@qq.com>
2017-05-10 11:54:56 +0800

    huishi
    加页面

---
+++ resources/views/hs/huishi.blade.php
@@ -0,0 +1,13 @@
+@extends('hs.layouts')
+
+@section('content')
+		<div class="page8">
+			<div class="c">
+				<img src="{{asset('img3/finish.png'}}"/>
+			</div>
+		</div>
+@endsection
+
+@section('script')
+
+@endsection
